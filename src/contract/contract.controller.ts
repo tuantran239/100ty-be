daeca9d50cdd09725c202ee87f71fe560ede5535
tuantran239 <tuanphamtran99@gmail.com>
2024-05-20 11:37:18 +0700

    Update api contract status

--- src/contract/contract.controller.ts
+++ src/contract/contract.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Post, Res } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Res,
+  Req,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import RouterUrl from 'src/common/constant/router';
 import { ContractService } from './contract.service';
-import { Response } from 'express';
+import { Request, Response } from 'express';
+import { ResponseData } from 'src/common/interface';
 
 @Controller(RouterUrl.CONTRACT.ROOT)
 export class ContractController {
   constructor(private contractService: ContractService) {}
 
   @Post(RouterUrl.CONTRACT.UPDATE_STATUS)
-  async updateStatus(@Res() res: Response) {}
+  async updateStatus(@Res() res: Response, @Req() req: Request) {
+    try {
+      const { contractType } = req.body;
+
+      const data =
+        await this.contractService.updateStatusContract(contractType);
+
+      const responseData: ResponseData = {
+        message: 'success',
+        data,
+        error: null,
+        statusCode: 200,
+      };
+
+      return res.status(200).send(responseData);
+    } catch (error: any) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
 }
